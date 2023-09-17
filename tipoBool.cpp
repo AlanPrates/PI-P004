79fb603edc87f663ed3631e6fec87df0a06c3adc
Lucas Santiago <lucascarmo2811@gmail.com>
2023-09-17 20:27:17 -0300

    Adicionando o exercício 5 - variáveis bool

--- tipoBool.cpp
+++ tipoBool.cpp
@@ -1,55 +1,40 @@
-/*Exercício 5: Variáveis de tipo Bool
-● As variáveis de tipo bool podem assumir valores true ou false. Elas
-serão amplamente utilizadas nos próximos tópicos. Entretanto, outros tipos
-de dados podem ser utilizados no lugar de valores bool e seus valores são
-convertidos em true ou false. 
-a. Faça uma aplicação que mostre como os diversos tipos que foram
-estudados até aqui são convertidos em true ou false.*/
+/*
+    Exercício 5: Variáveis de tipo Bool.
+    As variáveis de tipo bool podem assumir valores true ou false. Elas
+    serão amplamente utilizadas nos próximos tópicos. Entretanto, outros tipos
+    de dados podem ser utilizados no lugar de valores bool e seus valores são
+    convertidos em true ou false.
+
+    a. Faça uma aplicação que mostre como os diversos tipos que foram
+    estudados até aqui são convertidos em true ou false.
+*/
+
 #include <iostream>
 
 using namespace std;
 
-int main() {
-    // Tipos numéricos
-    int numeroInteiro = 42;
-    float numeroFloat = 0.0;
-    double numeroDouble = 3.14159;
+int main(){
 
-    if (numeroInteiro) {
-        cout << "O valor de numeroInteiro é avaliado como true." << endl;
-    } else {
-        cout << "O valor de numeroInteiro é avaliado como false." << endl;
-    }
+    int testeInt = 1;
+    float testeFloat = 2.0;
+    double testeDouble = 3.0;
+    char testeChar = 'a';
+    long int testeLI = 9223372036854775807;
 
-    if (numeroFloat) {
-        cout << "O valor de numeroFloat é avaliado como true." << endl;
-    } else {
-        cout << "O valor de numeroFloat é avaliado como false." << endl;
-    }
+    // Dependendo da condição a estrutura vai retornar 1 ou 0 -> True ou False.
 
-    if (numeroDouble) {
-        cout << "O valor de numeroDouble é avaliado como true." << endl;
-    } else {
-        cout << "O valor de numeroDouble é avaliado como false." << endl;
-    }
-
-    // Tipo char
-    char caractere = 'A';
-
-    if (caractere) {
-        cout << "O valor de caractere é avaliado como true." << endl;
-    } else {
-        cout << "O valor de caractere é avaliado como false." << endl;
-    }
-
-    // Tipo bool
-    bool valorBool = true;
-
-    if (valorBool) {
-        cout << "O valor de valorBool é avaliado como true." << endl;
-    } else {
-        cout << "O valor de valorBool é avaliado como false." << endl;
-    }
+    //               teste ? se verdadeiro : se falso
+    bool isInt = (testeInt == 1) ? true : false; // Verdadeiro! Retorna 1.
+    bool isFloat = (testeFloat == 0.0) ? true : false; // Falso! Retorna 0.
+    bool isChar = (testeChar == 'a') ? true : false; // Verdadeiro! Retorna 1.
+    bool isLongInt = (testeLI == -1) ? true : false; // Falso! Retorna 0.
+    bool isDouble = (testeDouble == 3.0) ? true : false; // Verdadeiro! Retorna 1.
+                   
+    cout << "Teste com o tipo int, valor em bool: " << isInt << endl; 
+    cout << "Teste com o tipo float, valor em bool: " << isFloat << endl;
+    cout << "Teste com o tipo char, valor em bool: " << isChar << endl;  
+    cout << "Teste com o tipo long int, valor em bool: " << isLongInt << endl;
+    cout << "Teste com o tipo double, valor em bool: " << isDouble << endl;
 
     return 0;
 }
