98f772a4957f1e4ec61c551046bbffbc6d425b9d
Lucas Santiago <lucascarmo2811@gmail.com>
2023-09-17 20:24:33 -0300

    Adicionando o exercício 4 - limites float

--- floatDoublePi.cpp
+++ floatDoublePi.cpp
@@ -1,100 +1,84 @@
 /*
-Durante a aula foram apresentados o tipo de dados double e suas
-variações, que permitem representar um subconjunto dos números reais.
-Sobre estes tipos de dados crie uma aplicação que:
-a. Sobre o tipo float identifique qual o menor e o maior valor que pode
-ser representado por uma variável deste tipo. Mostre esta informação na
-tela.
+    Exercício 4: Manipulação de variáveis de ponto flutuante, explorando os limites.
 
-b. Crie uma variável pif de tipo float e atribua a ela o valor da
-constante pi com o maior número de casas decimais que você consiga
-achar na Internet. Imprima este valor na tela.
+    a. Sobre o tipo float identifique qual o menor e o maior valor que
+    pode ser representado por uma variável deste tipo. Mostre esta
+    informação na tela.
 
-c. Explore as formas de formatar a saída com o comando cout e
-modifique a quantidade de casas decimais que são apresentadas, na
-impressão do valor de pi, para 2, 4, 8 e 16.
+    b. Crie uma variável pif de tipo float e atribua a ela o valor da
+    constante pi com o maior número de casas decimais que você consiga
+    achar na Internet. Imprima este valor na tela.
 
-d. Crie uma variável pid de tipo double e atribua a ela o valor da
-constante pi com o maior número de casas decimais que você consiga
-achar na Internet. Imprima este valor na tela.
+    c. Explore as formas de formatar a saída com o comando cout e
+    modifique a quantidade de casas decimais que são apresentadas para
+    2, 4, 8 e 16.
 
-e. Identifique se existe alguma diferença entre o valor pif e pid quando
-seus valores são impressos com cout utilizando 2, 4, 8 e 16 casas decimais
+    d. Crie uma variável pid de tipo double e atribua a ela o valor da
+    constante pi com o maior número de casas decimais que você consiga
+    achar na Internet. Imprima este valor na tela.
 
-f. No seu sistema existe diferença entre o tipo double e long double?
-Mostre seu achado com algum exemplo dentro do seu código.*/
+    e. Identifique se existe alguma diferença entre o valor pif e pid quando
+    seus valores são impressos com cout utilizando 2, 4, 8 e 16 casas
+    decimais
+
+    f. No seu sistema existe diferença entre o tipo double e long double?
+    Mostre seu achado com algum exemplo dentro do seu código.
+*/
 
 #include <iostream>
-#include <string>
-#include <math.h>
 #include <iomanip>
-
-
-//O #include <limits> incluir funcionalidade para obter, 
-//o mínimo e máximo de um tipo de variável 
 #include <limits>
 
 using namespace std;
 
+int main(){
+    // Resolução da letra 'a'.
+    cout << "O maior valor que pode ser representado por uma variável float é: " << numeric_limits<float>::max() << endl;
+    cout << "O menor valor que pode ser representado por uma variável float é: " << numeric_limits<float>::min() << endl;
 
-int main(void)
-{
+    cout << "\t===============--=-=-=-=-=-=--===============\n";
 
-// Declarando a variável "pif" do tipo float
-float pif = 3.14159265358979323846;
+    // Resolução da letra 'b' e 'c'.
+    float pif = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912;
 
+    cout << "Valor de pi: " << pif << endl;
+    cout << setprecision(2) << fixed;
+    cout << "Valor de pif (2 casas decimais): " << pif << endl;
+    cout << setprecision(4) << fixed;
+    cout << "Valor de pif (4 casas decimais): " << pif << endl;
+    cout << setprecision(8) << fixed;
+    cout << "Valor de pif (8 casas decimais): " << pif << endl;
+    cout << setprecision(16) << fixed;
+    cout << "Valor de pif (16 casas decimais): " << pif << endl;
 
-//Imprimindo na tela o mínimo e o máximo de um tipo float.
-cout << "Minimum value for float: " << numeric_limits<float>::min() << endl;
-cout << "Maximum value for float: " << numeric_limits<float>::max() << endl; 
+    // Resolução da letra 'd'.
+    cout << "\t===============--=-=-=-=-=--===============\n";
 
+    double pid = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912;
 
-//Imprimindo valor da variável pif na tela.
+    cout << setprecision(-1);
+    cout << "Valor de pi (double): " << pid << endl;
+    cout << setprecision(2) << fixed;
+    cout << "Valor de pid (2 casas decimais): " << pif << endl;
+    cout << setprecision(4) << fixed;
+    cout << "Valor de pid (4 casas decimais): " << pif << endl;
+    cout << setprecision(8) << fixed;
+    cout << "Valor de pid (8 casas decimais): " << pif << endl;
+    cout << setprecision(16) << fixed;
+    cout << "Valor de pid (16 casas decimais): " << pif << endl;
 
-cout << "Valor de pi: " << pif << endl;
+    // Resposta da letra 'e': sem diferenças!
 
-// Imprimindo o valor da variável pif com 2, 4, 8 e 16 casas decimais.
-cout << "Valor de pi duas casas decimais:" << setprecision (2) << pif << endl;
-cout << "Valor de pi quatro casas decimais:" << setprecision (4) << pif << endl;
-cout << "Valor de pi oito casas decimais:" << setprecision (8) << pif << endl;
-cout << "Valor de pi dezesseis casas decimais:" << setprecision (16) << pif << endl;
-cout<< endl;
-cout<< endl;
+    // Resolução da letra 'f'.
+    cout << "\t===============--=-=-=-=-=--===============\n";
+    cout << setprecision(-1); // Retornando ao padrão, pois estava no setprecision(16).
 
-cout << "################### Trabalhando com double.################" << endl;
-cout<< endl;
-// Declarando a variável pid do tipo double.
-double pid = 3.14159265358979323846;
+    long double pild = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912;
 
-//Imprimindo na tela o valor da variável pid.
-cout << "Valor de pi em double: " << pid << endl;
+    cout << "PI em long double: " << pild << endl;
+    // Testando a quantidade de bytes: o long double possui o dobro de armazenamento, mas, para mim, não mostrou diferença na impressão da constante PI. 
+    cout << "Quantidade de bytes do long double: " << sizeof(pild) << endl;
+    cout << "Quantidade de bytes do double: " << sizeof(pid) << endl;
 
-
-
-//Imprimindo valores da variável pid com 2, 4 ,8 e 16 casas decimais,
-// para identificando se existe diferença iem comparação com a variável pif.
-
-cout << "Valor de pi duas casas decimais:" << setprecision (2) << pid << endl;
-cout << "Valor de pi quatro casas decimais:" << setprecision (4) << pid << endl;
-cout << "Valor de pi oito casas decimais:" << setprecision (8) << pid << endl;
-cout << "Valor de pi dezesseis casas decimais:" << setprecision (16) << pid << endl;
-cout<< endl;
-cout<< endl;
-
-cout << "#################### Trabalhando com long double##################";
-cout<< endl;
-cout<< endl;
-long double pid2 = 3.14159265358979323846;
-
-//Imprimindo na tela o valor da variável pid2.
-cout << "Valor de pi em long double: " << pid2 << endl;
-cout<< endl;
-cout << "Valor de pi duas casas decimais:" << setprecision (2) << pid2 << endl;
-cout << "Valor de pi quatro casas decimais:" << setprecision (4) << pid2 << endl;
-cout << "Valor de pi oito casas decimais:" << setprecision (8) << pid2 << endl;
-cout << "Valor de pi dezesseis casas decimais:" << setprecision (16) << pid2 << endl;
-cout<< endl;
-
-return(0);
-
+    return 0;
 }
